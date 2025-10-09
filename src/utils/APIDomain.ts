a3add41af35f7720df92d67919a9e07b4ef10910
johnson mwangi <johnsonthuraniramwangi@gmail.com>
2025-10-09 18:07:50 +0300

    updates

--- src/utils/APIDomain.ts
+++ src/utils/APIDomain.ts
@@ -1,4 +1,5 @@
 // local host
 //export const APIDomain = 'http://localhost:3000/api/v1'
 // Example production API endpoint for @mwalimu (uncomment and set when deploying)
+// export const APIDomain = 'https://api.mwalimu.io/api/v1'
  export const APIDomain = 'https://atmwalimuapi.onrender.com/api/v1'
